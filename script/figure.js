80d483122af1dbb60043539f7406bba6951052d3
Vladimir <shues@bk.ru>
2020-12-23 09:01:45 +0300

    php cleared

--- script/figure.js
+++ script/figure.js
@@ -60,8 +60,6 @@ function draw() {
 
 function createFigure(source) {
   let geom = new THREE.Geometry();
-  //  addVertices(geom.vertices, source);
-  //  addFaces(geom.faces, source);
   addTriangles(geom, source);
   geom.computeFaceNormals();
   geom.computeVertexNormals();
@@ -85,52 +83,6 @@ function createFigure(source) {
   animate();
 }
 
-
-
-
-function addVertices(vertice, source) {
-  for (let face in source) {
-    let faceObj = source[face];
-    for (let tri in faceObj) {
-      let triObj = faceObj[tri];
-      for (let vert in triObj) {
-        let vertObj = triObj[vert];
-        let a = new THREE.Vector3(vertObj.height, vertObj.width, vertObj.deep);
-        if (vert === undefined) {
-          let a = new THREE.Vector3(0, 0, 0);
-        }
-
-        vertice.push(a);
-      }
-    }
-  }
-
-}
-
-function addFaces(faces, source) {
-  const normal = new THREE.Vector3(0, 0, 1);
-  const color = new THREE.Color(0xffaa00);
-  const materialIndex = 0;
-
-  let counter = 0;
-  for (let face in source) {
-    faces.push(new THREE.Face3((counter), (counter + 1), (counter + 2), normal, color, materialIndex));
-    counter += 3;
-  }
-  //  const face_0 = new THREE.Face3(0, 1, 2, normal, color, materialIndex);
-  //  const face_1 = new THREE.Face3(3, 4, 5, normal, color, materialIndex);
-  //  const face_2 = new THREE.Face3(6, 7, 8, normal, color, materialIndex);
-  //  const face_3 = new THREE.Face3(9, 10, 11, normal, color, materialIndex);
-  //  const face_4 = new THREE.Face3(12, 13, 14, normal, color, materialIndex);
-  //  const face_5 = new THREE.Face3(15, 16, 17, normal, color, materialIndex);
-  //
-  //  faces.push(face_0);
-  //  faces.push(face_1);
-  //  faces.push(face_2);
-  //  faces.push(face_3);
-  //  faces.push(face_4);
-  //  faces.push(face_5);
-}
 
 function addTriangles(geom, triangleSet) {
   const normal = new THREE.Vector3(0, 0, 1);
